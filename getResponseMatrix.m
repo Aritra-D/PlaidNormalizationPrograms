afdba5d4daa2ec6961654d72ccdd188d6e8cd428
Aritra <aritradas.2012@gmail.com>
2019-04-22 10:37:55 +0530

    Basic fitting electrodeWise for all measures

--- getResponseMatrix.m
+++ getResponseMatrix.m
@@ -16,18 +16,19 @@ for i=1:length(c1List)
     c1=c1List(i);
     for j=1:length(c2List)
         c2 = c2List(j);
-        predictedResponseMatrix(i,j) = (c1*L1+c2*L2)/(alpha1*c1+alpha2*c2+sigma); %#ok<AGROW>
+        predictedResponseMatrix(i,j) = (c1*L1+c2*L2)/(alpha1*(c1)+alpha2*(c2)+sigma); %#ok<AGROW>
+%         predictedResponseMatrix(i,j) = ((c1)^2*L1+(c2)^2*L2)/(alpha1*(c1)^2+alpha2*(c2)^2+(sigma)^2); %#ok<AGROW>
     end
 end
 
 predictedResponseMatrix = flip(predictedResponseMatrix,1);
 
-subplot(2,2,1)
-plot(c1List,actualResponseMatrix(end,:),'Marker','o','LineStyle','none','color','r')
-hold on;
-plot(c1List,predictedResponseMatrix(end,:),'LineWidth',2,'color','r')
-plot(c1List,diag(flipud(actualResponseMatrix)),'Marker','o','LineStyle','none','color','b')
-plot(c1List,diag(flipud(predictedResponseMatrix)),'LineWidth',2,'color','b')
+% subplot(2,2,1)
+% plot(c1List,actualResponseMatrix(end,:),'Marker','o','LineStyle','none','color','r')
+% hold on;
+% plot(c1List,predictedResponseMatrix(end,:),'LineWidth',2,'color','r')
+% plot(c1List,diag(flipud(actualResponseMatrix)),'Marker','o','LineStyle','none','color','b')
+% plot(c1List,diag(flipud(predictedResponseMatrix)),'LineWidth',2,'color','b')
 
 
 
